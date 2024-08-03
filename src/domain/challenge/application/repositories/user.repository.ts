52a4a44c1e6a251cf28bc3b6d292d746a8a48b49
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-03 14:23:41 -0300

    gkissel : feat add user repository entity

---
+++ src/domain/challenge/application/repositories/user.repository.ts
@@ -0,0 +1,9 @@
+import { User } from '../../enterprise/user'
+
+export abstract class UsersRepository {
+  abstract create(user: User): Promise<void>
+  abstract delete(user: User): Promise<void>
+  abstract save(user: User): Promise<void>
+
+  abstract findByEmail(email: string): Promise<User>
+}
