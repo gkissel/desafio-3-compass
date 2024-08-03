df5a51994ef5690ff6630c5a68250f22047ed6b4
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-03 14:11:41 -0300

    gkissel : feat add Event entity

---
+++ src/domain/challenge/enterprise/event.ts
@@ -0,0 +1,38 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+type weekDay =
+  | 'sunday'
+  | 'monday'
+  | 'tuesday'
+  | 'wednesday'
+  | 'thursday'
+  | 'friday'
+  | 'saturday'
+
+interface EventProps {
+  description: string
+  dayOfWeek: weekDay
+
+  userId: UniqueEntityID
+}
+
+export class Event extends Entity<EventProps> {
+  get description(): string {
+    return this.props.description
+  }
+
+  get dayOfWeek(): string {
+    return this.props.dayOfWeek
+  }
+
+  get userId(): UniqueEntityID {
+    return this.props.userId
+  }
+
+  static create(props: EventProps, id?: UniqueEntityID) {
+    const event = new Event(props, id)
+
+    return event
+  }
+}
