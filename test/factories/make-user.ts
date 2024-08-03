50b6563678fa9e875ab7e5ccd1e49011b31d2fbe
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-03 15:28:29 -0300

    gkissel : feat add user factory

---
+++ test/factories/make-user.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { User, UserProps } from '@/domain/challenge/enterprise/user'
+
+export function makeUser(
+  override: Partial<UserProps> = {},
+  id?: UniqueEntityID,
+) {
+  const user = User.create(
+    {
+      email: faker.internet.email(),
+      firstName: faker.person.firstName(),
+      lastName: faker.person.lastName(),
+      password: faker.internet.password(),
+      birthDate: faker.date.birthdate().toISOString(),
+      city: faker.location.city(),
+      country: faker.location.country(),
+      ...override,
+    },
+    id,
+  )
+
+  return user
+}
