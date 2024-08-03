bf5bec6b72e3c25342cfb3c6238cf6229913c8ee
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-03 15:28:06 -0300

    gkissel : feat update user entity

--- src/domain/challenge/enterprise/user.ts
+++ src/domain/challenge/enterprise/user.ts
@@ -1,7 +1,7 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
-interface UserProps {
+export interface UserProps {
   firstName: string
   lastName: string
   birthDate: string
