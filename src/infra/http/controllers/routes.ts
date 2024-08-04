4add6ac9998dca6aa4f1ea2f03dba936c6d97fea
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-04 18:59:38 -0300

    gkissel : feat update api router to include event router

--- src/infra/http/controllers/routes.ts
+++ src/infra/http/controllers/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express'
 
-// import UserRouter from './users/users.routes'
+import EventRouter from './event/event.routes'
+import UserRouter from './user/users.routes'
 
 const routes = Router({
   mergeParams: true,
 })
 
-// routes.use('/user', UserRouter)
+routes.use('/user', UserRouter)
+routes.use('/events', EventRouter)
 
 export default routes
