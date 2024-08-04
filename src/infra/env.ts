67bf6c1d1bfda7257ae0d941c11380a272e3a392
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-04 18:51:42 -0300

    gkissel : feat update env

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -3,15 +3,17 @@ import * as j from 'joi'
 
 dotenv.config()
 
-const envSchema = j.object({
+export const envSchema = j.object({
   PORT: j.number().optional().default(3333),
-  JWT_SECRET: j.string,
+  JWT_SECRET: j.string(),
+  DATABASE_URL: j.string(),
 })
 
 // Odeio Joi
-type envType = {
+export type envType = {
   PORT: number
   JWT_SECRET: string
+  DATABASE_URL: string
 }
 
 const { value } = envSchema.validate(process.env)
