5c232c0b11c362154f979b2e158db3a76733e02f
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-05 17:16:26 -0300

    gkissel : feat Add EventFactory class for creating events with Prisma

--- test/factories/make-event.ts
+++ test/factories/make-event.ts
@@ -1,9 +1,10 @@
 import { faker } from '@faker-js/faker'
-import { DayOfWeek } from '@prisma/client'
+import { DayOfWeek, PrismaClient } from '@prisma/client'
 import { randomInt } from 'crypto'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Event, EventProps } from '@/domain/challenge/enterprise/event'
+import { PrismaEventMapper } from '@/infra/database/prisma/mappers/prisma-event.mapper'
 
 export function makeEvent(
   override: Partial<EventProps> = {},
@@ -29,4 +30,18 @@ export function makeEvent(
   )
 
   return event
+}
+
+export class EventFactory {
+  constructor(private prisma: PrismaClient) {}
+
+  async makePrismaEvent(data: Partial<EventProps> = {}): Promise<Event> {
+    const event = makeEvent(data)
+
+    await this.prisma.event.create({
+      data: PrismaEventMapper.toPrisma(event),
+    })
+
+    return event
+  }
 }
