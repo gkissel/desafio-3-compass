f5c84e9a5e4ffecdec7760ce33810e6647346356
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-05 17:17:09 -0300

    gkissel : feat Add e2e test for deleting event by ID

---
+++ test/controllers/event/delete-event-by-id.e2e-spec.ts
@@ -0,0 +1,34 @@
+import { EventFactory } from '@test/factories/make-event'
+import { UserFactory } from '@test/factories/make-user'
+import { hash } from 'bcryptjs'
+import supertest from 'supertest'
+
+import { JwtEncrypter } from '@/infra/cryptography/jwt-encrypter'
+import { prisma } from '@/server'
+
+const request = supertest('http://localhost:3000')
+
+describe('Delete event by id (e2e)', () => {
+  const userFactory = new UserFactory(prisma)
+
+  const eventFactory = new EventFactory(prisma)
+
+  const jwtEncrypter = new JwtEncrypter()
+  it('should be able to delete a event', async () => {
+    const user = await userFactory.makePrismaUser({
+      password: await hash('123456', 8),
+    })
+
+    const event = await eventFactory.makePrismaEvent()
+
+    const response = await request
+      .delete(`/api/v1/events/${event.id.toString()}`)
+      .withCredentials(true)
+      .set(
+        'Authorization',
+        `Bearer ${await jwtEncrypter.encrypt({ sub: user.id.toString() })}`,
+      )
+
+    expect(response.statusCode).toEqual(200)
+  })
+})
