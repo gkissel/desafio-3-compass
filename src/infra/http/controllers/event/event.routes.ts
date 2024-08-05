eacfc5ece32755d3d8ddf170148e7154caa9adf2
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-05 17:16:36 -0300

    gkissel : feat Add CRUD endpoints for events

--- src/infra/http/controllers/event/event.routes.ts
+++ src/infra/http/controllers/event/event.routes.ts
@@ -2,12 +2,24 @@ import { Router } from 'express'
 
 import { verifyToken } from '../../middlewares/auth-middleware'
 import { CreateEventController } from './create'
+import { DeleteEventByIdController } from './delete-by-id'
+import { DeleteManyEventsController } from './delete-many'
+import { GetEventByIdController } from './get-by-id'
+import { GetManyEventsController } from './get-many'
 
 const EventRouter = Router({
   mergeParams: true,
 })
 const createEventController = new CreateEventController()
+const getManyEventsController = new GetManyEventsController()
+const deleteManyEventsController = new DeleteManyEventsController()
+const getEventByIdController = new GetEventByIdController()
+const deleteEventByIdController = new DeleteEventByIdController()
 
 EventRouter.post('/', verifyToken, createEventController.execute)
+EventRouter.get('/', verifyToken, getManyEventsController.execute)
+EventRouter.delete('/', verifyToken, deleteManyEventsController.execute)
+EventRouter.get('/:id', verifyToken, getEventByIdController.execute)
+EventRouter.delete('/:id', verifyToken, deleteEventByIdController.execute)
 
 export default EventRouter
