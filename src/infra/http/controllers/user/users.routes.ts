986fb1b10a0c609f7e0a09f76cd1cf62a2f7995b
Gustavo Kissel <kisselgustavo@gmail.com>
2024-08-07 17:15:28 -0300

    feat: Add AddUserProfileController for adding user profile
    The code changes in the `users.routes.ts` file add an import statement for the `AddUserProfileController` and a new route `/add-profile` to the `UserRouter`. This allows users to add their profile information, including an avatar URL.
    
    Recent commits:
    - Add AddUserProfileService for adding user profile with avatar URL
    - Add avatarUrl field to User model and PrismaUserMapper
    - Add required AWS_BUCKET_NAME environment variable
    - Add avatarUrl field to User model and update User class
    - Update npm dependencies
    - Add AWS SDK dependency for S3 integration
    - Update Docker Compose version to 3.3 and remove commented code in Dockerfile
    - Add Dockerfile and .dockerignore for containerization
    - Add e2e test for getting many events with optional filters
    - Add e2e test for deleting multiple events by day of week
    - Add e2e test for deleting event by ID
    - Add e2e test for getting event by ID
    - Update create-event.e2e-spec.ts to use 'create' instead of 'authenticate'
    - Add CRUD endpoints for events
    - Add EventFactory class for creating events with Prisma
    - Add GetManyEventsController for retrieving multiple events with optional filters

--- src/infra/http/controllers/user/users.routes.ts
+++ src/infra/http/controllers/user/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 
+import { AddUserProfileController } from './add-user-profile'
 import { CreateUserController } from './create'
 import { AuthenticateUserController } from './login'
 
@@ -8,8 +9,10 @@ const UserRouter = Router({
 })
 const createUserController = new CreateUserController()
 const authenticateUserController = new AuthenticateUserController()
+const addUserProfileController = new AddUserProfileController()
 
 UserRouter.post('/sign-up', createUserController.execute)
 UserRouter.post('/sign-in', authenticateUserController.execute)
+UserRouter.post('/add-profile', addUserProfileController.execute)
 
 export default UserRouter
